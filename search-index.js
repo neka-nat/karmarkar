9912345fe9710ece605bf84d0ba5d24ea3f7c7c0
nekanat <nekanat.stock@gmail.com>
2021-08-21 13:01:59 +0900

    gh-pages

---
+++ search-index.js
@@ -0,0 +1,4 @@
+var searchIndex = JSON.parse('{\
+"karmarkar":{"doc":"","t":[0,5],"n":["karmarkar","karmarkar"],"q":["karmarkar","karmarkar::karmarkar"],"d":["",""],"i":[0,0],"f":[null,[[["dvector",6],["dmatrix",6],["f64",15],["i32",15]],[["dvector",6],["str",15],["result",4]]]],"p":[]}\
+}');
+initSearch(searchIndex);
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["karmarkar"] = {"name":"","files":["karmarkar.rs","lib.rs"]};
+createSourceSidebar();
